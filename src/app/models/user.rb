7852ec690aa44824c8311a149c586119ccb2197f
kzm-sris <sriskzm@gmail.com>
2023-02-07 11:44:27 +0900

    MODIFIED test

--- src/app/models/user.rb
+++ src/app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   has_many :post_books, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   has_one_attached :profile_image
+  validates :email, uniqueness: true
 
   def get_profile_image(width, height)
     unless profile_image.attached?
